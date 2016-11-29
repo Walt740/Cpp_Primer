a46b6b1dab249d8a853f639a8787d2a9349d3628
BinBinStory <yinhao1033@163.com>
2016-11-29 23:42:08 +0800

    补充

---
+++ 第14章 操作重载与类型转换/14.19/Date.h
@@ -0,0 +1,29 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+
+class Date
+{
+    friend std::istream& operator>>(std::istream &is,Date &dt);
+    friend std::ostream& operator<<(std::ostream &os,const Date &dt);
+    friend bool operator==(const Date &lhs,const Date &rhs);
+    friend bool operator!=(const Date &lhs,const Date &rhs);
+
+    friend bool operator< (const Date &lhs,const Date &rhs);
+    friend bool operator<=(const Date &lhs,const Date &rhs);
+    friend bool operator> (const Date &lhs,const Date &rhs);
+    friend bool operator>=(const Date &lhs,const Date &rhs);
+
+
+    public:
+        Date()=default;
+        Date(int y,int m,int d); //{year = y;month = m; day = d;}
+
+    private:
+        int year,month,day;
+};
+
+
+
+#endif // DATE_H
