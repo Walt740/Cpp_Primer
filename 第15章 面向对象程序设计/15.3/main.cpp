999dfd89458c4e9b42f6e3576b050e5fa66423b4
BinBinStory <yinhao1033@163.com>
2016-12-03 00:16:08 +0800

    15.3、15.5、15.6、15.7

--- 第15章 面向对象程序设计/15.3/main.cpp
+++ 第15章 面向对象程序设计/15.3/main.cpp
@@ -12,7 +12,7 @@ public:
 	{}
 
 	std::string isbn() const { return bookNo; }
-	virtual double net_price(std::size_t n) const { return n * price; }
+	virtual double net_price(std::size_t n) const { cout << "调用基类" <<endl ; return n * price; }
 
 	virtual ~Quote() = default;
 
@@ -29,9 +29,10 @@ class Bulk_quote:public Quote  //Bulk_quote继承自Quote
 {
 public:
     Bulk_quote() = default;
-    Bulk_quote(const std::string &,double ,std::size_t,double);
+    Bulk_quote(const std::string &book,double p,std::size_t qty,double disc):
+        Quote(book,p),min_qty(qty),discount( disc) {}
     //覆盖基类的函数版本以实现基于大量购买的折扣政策
-    double net_price(std::size_t) const override;
+    virtual double net_price(std::size_t) const override;
 private:
     std::size_t min_qty = 0; //适用折扣政策的最低购买量
     double discount = 0.0; //以小数表示的折扣额
@@ -39,6 +40,7 @@ private:
 
 double Bulk_quote::net_price(size_t cnt) const
 {
+    cout << "调用派生类" <<endl ;
     if(cnt >= min_qty)
         return cnt * (1- discount)* price;
     else
@@ -47,18 +49,6 @@ double Bulk_quote::net_price(size_t cnt) const
 
 
 double print_total(std::ostream& os, const Quote& item, size_t n);
-
-int main()
-{
-    Quote quo;
-    Bulk_quote bulk;
-    quo.
-
-    bulk.bookNo
-	return 0;
-}
-
-
 double print_total(std::ostream &os, const Quote &item, size_t n)
 {
 	double ret = item.net_price(n);
@@ -68,6 +58,58 @@ double print_total(std::ostream &os, const Quote &item, size_t n)
 
 	return ret;
 }
+
+
+class Limited_quote:public Bulk_quote
+{
+public:
+    Limited_quote() = default;
+    Limited_quote(const std::string &book,double p,std::size_t qty,double disc):
+                      Bulk_quote(book,p,qty,disc),min_qty(qty),discount( disc) {}
+    double net_price(size_t cnt) const override
+    {
+        cout <<"Limited_quote "<<endl;
+        if(cnt <= min_qty)
+            return cnt * (1 - discount)*price;
+        else
+            return min_qty*(1-discount)*price + (cnt - min_qty)*price;
+    }
+private:
+    size_t min_qty;
+    double discount;
+};
+
+//class Limited_quote:public  Quote
+//{
+//public:
+//    Limited_quote() = default;
+//    Limited_quote(const std::string &book,double p,std::size_t qty,double disc):
+//        Quote(book,p),min_qty(qty),discount( disc) {}
+//    double net_price(size_t cnt) const override
+//    {
+//        if(cnt <= min_qty)
+//            return cnt * (1 - discount)*price;
+//        else
+//            return min_qty*(1-discount)*price + (cnt - min_qty)*price;
+//    }
+//private:
+//    size_t min_qty;
+//    double discount;
+//};
+
+int main()
+{
+    Quote quo("C++ Primer",30);
+    Bulk_quote bulk("C++ Primer",30,20,0.1); //折扣10%
+    Limited_quote liq("C++ Primer",30,20,0.1); //折扣10%
+    print_total(cout,quo,20);
+    print_total(cout,bulk,10);
+    print_total(cout,liq,30);
+	return 0;
+}
+
+
+
 
 
 
