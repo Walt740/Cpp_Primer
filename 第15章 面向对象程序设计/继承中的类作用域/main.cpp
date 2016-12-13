c4d22e891ac1cfdf1746031fdad044f1d64cc39f
BinBinStory <yinhao1033@163.com>
2016-12-14 00:48:49 +0800

    20161214

--- 第15章 面向对象程序设计/继承中的类作用域/main.cpp
+++ 第15章 面向对象程序设计/继承中的类作用域/main.cpp
@@ -2,6 +2,9 @@
 #include <string>
 using namespace std;
 
+
+
+/*
 //基类
 class Quote
 {
@@ -9,17 +12,12 @@ public:
 	Quote() = default;
 	Quote(const std::string &b, double p) :
 		bookNo(b), price(p)
-	{
-		cout << "基类的构造函数" << endl;
-	}
+	{}
 
 	std::string isbn() const { return bookNo; }
 	virtual double net_price(std::size_t n) const { cout << "调用基类" <<endl ; return n * price; }
-	//如果我们删除的是一个指向派生类对象的基类指针，则需要虚析构函数
-	virtual ~Quote()
-	{
-	    cout << "基类的析构函数" << endl ;
-	}// = default;
+
+	virtual ~Quote() = default;
     virtual void debug()
     {
         cout << " bookNo = " << bookNo << " price = "<< price << endl;
@@ -31,20 +29,18 @@ protected:
 	double  price = 0.0;
 
 };
-//抽象基类
+
 class Disc_quote:public Quote
 {
 public:
     Disc_quote() = default;
     Disc_quote(const std::string& book,double price,
                std::size_t qty,double disc):
-                   Quote(book,price),quantity(qty),discount(disc){
-		cout << "抽象基类的构造函数" << endl;
-	}
-	~Disc_quote()// = delete;
-	{
-		cout << "抽象基类的析构函数" << endl;
-	}//=default;
+                   Quote(book,price),quantity(qty),discount(disc){}
+    std::pair<size_t,double> discount_policy() const
+    {
+        return {quantity,discount};
+    }
     virtual double net_price(std::size_t) const = 0;
 protected:
     std::size_t quantity = 0; //折扣适用的购买量
@@ -56,13 +52,7 @@ class Bulk_quote:public Disc_quote
 public:
     Bulk_quote() = default;
     Bulk_quote(const std::string &book,double price,std::size_t qty,double disc):
-        Disc_quote(book,price,qty,disc) {
-		cout << "派生类的构造函数" << endl;
-	}
-    ~Bulk_quote()
-    {
-        cout << "派生类的析构函数"<< endl ;
-    }
+        Disc_quote(book,price,qty,disc) {}
     //覆盖基类的函数版本以实现基于大量购买的折扣政策
     double net_price(std::size_t cnt) const override
     {
@@ -94,10 +84,10 @@ private:
 
 
 //double print_total(std::ostream& os, const Quote& item, size_t n);
-/*
-在C++中，当我们使用基类的引用（或指针）调用一个虚函数时将发生动态绑定
-我们既能使用基类Quote的对象调用该函数，也能使用派生类Bulk_quote的对象调用它；
-*/
+
+//在C++中，当我们使用基类的引用（或指针）调用一个虚函数时将发生动态绑定
+//我们既能使用基类Quote的对象调用该函数，也能使用派生类Bulk_quote的对象调用它；
+
 double print_total(std::ostream &os, const Quote &item, size_t n)
 {
 	double ret = item.net_price(n);
@@ -129,32 +119,39 @@ public:
 
 int main()
 {
-/*
-    Quote quo("C++ Primer",30);
-    Bulk_quote bulk("C++ Primer",30,20,0.1); //折扣10%
-    Limited_quote lq("C++ Primer",30,20,0.1);
-
-
-    print_total(cout,quo,20);
-    print_total(cout,bulk,10);
-    print_total(cout,lq,100);
-    quo.debug();
-    bulk.debug();
-*/
-    Quote *itemP = new  Quote;
-    delete itemP;
-    itemP = new Bulk_quote("yinhao",10,1,20);
-    delete itemP;
-
-
-	getchar();
+    Bulk_quote bulk;
+    Bulk_quote *bulkP = &bulk; //静态类型与动态类型一致
+    Quote *itemP = &bulk; //静态类型与动态类型不一致
+    bulkP->discount_policy();
+    itemP->discount_policy();
 	return 0;
 }
 
+*/
 
 
+struct Base
+{
+    Base():mem(0){}
+protected:
+    int mem;
 
+};
 
+struct Derived: public Base
+{
+    Derived(int i):mem(i){ }
+    int get_mem() {return  Base::mem;} //返回Derived::mem
+protected:
+    int mem; //隐藏基类中的mem
+};
+
+int main()
+{
+    Derived d(42);
+    cout << d.get_mem() << endl;
+    return 0;
+}
 
 
 
